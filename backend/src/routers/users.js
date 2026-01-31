ca6aff6b5e664b0d2d778bbf8c3a407f9eaf65ac
shine7on <pabboo13@gmail.com>
2026-01-31 17:28:51 -0600

    Add: added PUT to update the user data

--- backend/src/routers/users.js
+++ backend/src/routers/users.js
@@ -25,11 +25,31 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-// POST user
+// POST: Add the new user
 router.post("/", async (req, res) => {
   try {
-    const user = await User.create(req.body);
-    res.status(200).json(user);
+      const user = await User.create(req.body);
+      res.status(200).json(req.body);
+  } catch (error) {
+      console.error(error);
+      res.status(400).json({ Error: error.message });
+  }
+});
+
+// PUT: Update the user
+router.put("/:id", async (req, res) => {
+  try {
+    const {id} = req.params;
+
+    const user = await User.findByIdAndUpdate(id, req.body);
+
+    if (!user)
+      res.status(404).json({ message: "User not found" });
+
+    const UpdatedUser = await User.findById(id);
+
+    res.status(200).json(UpdatedUser);
+
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
