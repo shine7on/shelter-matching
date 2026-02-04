c3d1d6b2b6e0e72ae614526e72ccc500cd8d9bbe
shine7on <pabboo13@gmail.com>
2026-02-04 15:16:27 -0600

    Add: added API pagination for feeds

--- backend/src/controllers/feed.controller.js
+++ backend/src/controllers/feed.controller.js
@@ -6,14 +6,28 @@ export async function getFeed (req, res) {
         const { userId } = req.params;
 
         if (!userId)
-            res.status(404).json({Error: "User not found"});
+            return res.status(404).json({ error: "User not found" });
+
+        const limit = Math.min(parseInt(req.query.limit || "20", 10), 50);
+        const skip = Math.max(parseInt(req.query.skip || "0", 10), 0);
+
+        console.log("QUERY:", req.query);
 
         const swipes = await Swipe.find({userId}).select("dogId");
         const swipedDogsIds = swipes.map(s => s.dogId);
 
-        const dogs = await Dog.find({ _id: { $nin: swipedDogsIds }}).limit(20);
+        const dogs = await Dog.find({ _id: { $nin: swipedDogsIds }})
+            .skip(skip)
+            .limit(limit);
 
-        res.status(200).json(dogs);
+        res.status(200).json({
+            meta: {
+                limit,
+                skip,
+                returned: dogs.length,
+                hasMore: dogs.length === limit
+            },
+            dogs});
 
     } catch (error) {
         res.status(500).json({ error: error.message });
